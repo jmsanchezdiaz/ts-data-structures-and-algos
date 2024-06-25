64bd4c4fd58749f54a4bfc4b0a62f8ecfb3e3a92
jmsanchezdiaz <jmsanchezdiaz02@gmail.com>
2024-06-24 22:07:54 -0300

    add linkedlist file

---
+++ sorting/bubble_sort.ts
@@ -0,0 +1,11 @@
+export default function bubbleSort(numbers: number[]) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length - 1 - i; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        const temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+      }
+    }
+  }
+}
